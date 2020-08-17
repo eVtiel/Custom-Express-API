3bc64402c07319487a96fbdf0443fbe121b09cf3
Alex Shapovalov <evtiel.go@gmail.com>
2020-08-17 15:29:19 -0400

    Defining tracks, listing tracks, creating tracks

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodeParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
 app.use(bodeParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 const mongoUri = 'mongodb+srv://admin:1qaZxsw2@track-server.2you4.mongodb.net/<dbname>?retryWrites=true&w=majority';
 mongoose.connect(mongoUri, {
